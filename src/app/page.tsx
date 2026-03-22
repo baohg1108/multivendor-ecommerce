db3ccc1cc4f26beff78c28695770af202cb170ab
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-03-22 22:22:14 +0700

    feat: optimize metadata and add font configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-red-500">Hello, Next.js!</h1>
+      <h1 className=" text-red-500 font-barlow text-4xl">Hello, Next.js!</h1>
+      <h1 className=" text-red-500 text-4xl">Hello, Next.js!</h1>
     </div>
   );
 }
