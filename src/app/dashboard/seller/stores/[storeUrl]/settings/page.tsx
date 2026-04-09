29a0ca8e26d40571085f8d7469787553f6e2ebb6
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-04-09 22:42:57 +0700

    feat: setup store settings page

--- src/app/dashboard/seller/stores/[storeUrl]/settings/page.tsx
+++ src/app/dashboard/seller/stores/[storeUrl]/settings/page.tsx
@@ -1,3 +1,25 @@
-export default function SellerStoreSettingsPage() {
-  return <div>Seller Store Settings</div>;
+// DB
+import StoreDetails from "@/components/dashboard/forms/store-details";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export default async function SellerStoreSettingsPage({
+  params,
+}: {
+  params: Promise<{ storeUrl: string }>;
+}) {
+  const { storeUrl } = await params;
+
+  const storeDetails = await db.store.findUnique({
+    where: {
+      url: decodeURIComponent(storeUrl),
+    },
+  });
+  if (!storeDetails) redirect("/dashboard/seller/stores");
+  // console.log("params --> ", params);
+  return (
+    <div>
+      <StoreDetails data={storeDetails} />
+    </div>
+  );
 }
