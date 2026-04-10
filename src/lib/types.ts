6ab68df07e75c8f636774d2a526dab71858b7974
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-04-10 12:25:59 +0700

    feat: complete product form

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,4 @@
+import { SubCategory } from "@prisma/client";
 import { getAllSubCategories } from "@/queries/subCategory";
 import { Prisma } from "./../generated/prisma/index.d";
 export interface DashboardSidebarMenuInterface {
@@ -11,3 +12,23 @@ export interface DashboardSidebarMenuInterface {
 export type SubCategoryWithCategoryType = Prisma.PromiseReturnType<
   typeof getAllSubCategories
 >[0];
+
+// Product + variant
+export type ProductWithVariantType = {
+  productId: string;
+  variantId: string;
+  name: string;
+  description: string;
+  variantName: string;
+  variantDescription: string;
+  images: { url: string }[];
+  categoryId: string;
+  subCategoryId: string;
+  isSale: boolean;
+  sku: string;
+  colors: { color: string }[];
+  sizes: { size: string; quantity: number; price: number; discount: number }[];
+  keywords: string[];
+  createdAt: Date;
+  updatedAt: Date;
+};
